5387f5c71f6315ec8fe479aebeb329fe33c8dee6
zhuangzhuang52088 <1024312298@qq.com>
2021-04-14 16:10:09 +0800

    登录页面

---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+//import store from './store'
+
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router, //使用上vue-router
+ // store, // 使用上vuex
+})
